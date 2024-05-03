025c275d7d7f0f2d69e14348ed35ca79db817b33
Atharv Shete <49875547+AtharvShete@users.noreply.github.com>
2024-05-03 18:24:07 +0530

    Add files via upload

---
+++ mad-libs.py
@@ -0,0 +1,26 @@
+word1 = input("Describe the day with an adjective \n")
+word2 = input("Where does the adventure take place? \n")
+word3 = input("What's the name of the protagonist? \n")
+word4 = input("How would you describe the adventure? \n")
+word5 = input("What item does the protagonist carry with them? \n")
+word6 = input("What action does the protagonist aim to do? \n")
+word7 = input("What is the protagonist trying to find?\n")
+word8 = input("What obstacle do they encounter on their journey?\n")
+word9 = input("How does the obstacle try to stop them? \n")
+word10 = input("How would you describe the protagonist's luck? \n")
+word11 = input("What's the name of the protagonist again? \n")
+word12 = input("What did they use to outsmart the obstacle? \n")
+word13 = input("Where do they find themselves next? \n")
+word14 = input("Describe the forest they find. \n")
+word15 = input("What populates the forest? \n")
+word16 = input("What hidden thing do they discover in the forest? \n")
+word17 = input("What word describes the dragon guarding the secret? \n")
+word18 = input("What do they battle the dragon with? \n")
+word19 = input("What kind of battle do they engage in? \n")
+word20 = input("How would you describe the protagonist after their victory? \n")
+word21 = input("Where is the protagonist returning as a hero? \n")
+
+
+story = f"Once upon a time, in a {word1} land, there was a {word2} where a {word3} lived.They were known for their {word4} and their {word5}. They aimed to {word6} and find {word7}. However, they encountered {word8} that tried to {word9}. Despite their {word10} they managed to outsmart the {word8} with their {word12}. They found themselves in a {word13} and discovered a {word14} forest. The forest was populated with {word15} and they discovered a {word16}. The {word17} dragon guarded the secret, but they battled the dragon with {word18} in a {word19} battle. After their victory, they were {word20} and returned as a hero to {word21}."
+
+print("\n" + story)
